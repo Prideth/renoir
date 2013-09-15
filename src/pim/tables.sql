bcc902fd341abbc0415188daeb9d7bef85846fa1
lk <lk@192.168.2.101>
2013-09-15 20:43:04 +0200

    Skript zum erstellen der Datenbankstruktur

--- src/pim/tables.sql
+++ src/pim/tables.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE  `pim`.`users` (
+CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(12) NOT NULL DEFAULT '',
   `password` varchar(12) NOT NULL DEFAULT '',
